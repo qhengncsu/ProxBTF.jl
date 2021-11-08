d4e14b9af0d9b63a053f8e12bad7030ec2c687d3
qhengncsu <qheng@ncsu.edu>
2021-11-08 15:08:17 -0500

    1234

--- src/model_pbsrtf.jl
+++ src/model_pbsrtf.jl
@@ -71,7 +71,7 @@ function logdensity_and_gradient(problem::pbsrtfProblem, z)
     t = Variable()
     #p_gurobi = minimize(sumsquares(β-η)+sumsquares(α-t),norm(D*η,1)<=t)
     if restriction=="increasing"
-        p_gurobi = minimize(sumsquares(β-η)+sumsquares(α-t)+1000*norm(pos(-D¹*η),1),norm(D*η,1)<=t)
+        p_gurobi = minimize(sumsquares(β-η)+sumsquares(α-t)+(2*λ)*1000*norm(pos(-D¹*η),1),norm(D*η,1)<=t)
         #p_gurobi.constraints += D¹*η>=0
     elseif restriction=="decreasing"
         p_gurobi.constraints += D¹*η<=0
@@ -108,7 +108,8 @@ function logdensity_and_gradient(problem::pbsrtfProblem, z)
     qf       = (ssm+sse+2*r)/(2*σ²)
     logl     = -qf-(m/2+s)*logσ²+logα-μ*α
     dist²    = norm(β.-β_proj)^2+(α-α_proj)^2
-    logl    -= dist²/(2*λ)
+    diff     = -D¹*β_proj
+    logl    -= dist²/(2*λ)+1000*norm(diff.*(diff.>=0),1)
     ∇        = similar(z)
     ∇[1:n]   = (w.*res)./σ².-((β.-β_proj)./λ)
     ∇[n+1]   = qf-(m/2+s)
